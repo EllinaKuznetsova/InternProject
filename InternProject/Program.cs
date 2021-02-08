3823de07d34d93fb0b4105f8bd7dd2de5f582492
Ellina Kuznetsova <ellinakuznetsova@mail.ru>
2021-02-09 01:42:52 +0300

    Task 1. Создан базовый проект

---
+++ InternProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InternProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("World!");
+        }
+    }
+}
