ba71fcee538c23d447e8a5a03535c27a2e7d9ad6
Ellina Kuznetsova <ellinakuznetsova@mail.ru>
2021-02-09 03:19:53 +0300

    Task 2. Добавлены интерфейсы

---
+++ InternProject/IDownloader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InternProject
+{
+    /// <summary>
+    /// Интерфейс загрузчика
+    /// </summary>
+    public interface IDownloader
+    {
+        /// <summary>
+        /// Загрузка страницы
+        /// </summary>
+        /// <param name="url">Ссылка на страницу сайта</param>
+        /// <param name="path">Путь сохранения файла</param>
+        /// <returns>Успешно ли завершена загрузка</returns>
+        bool DownloadPage(string url, string path);
+    }
+}
---
+++ InternProject/IPrinter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InternProject
+{
+    /// <summary>
+    /// Интерфейс вывода строк
+    /// </summary>
+    public interface IPrinter
+    {
+        /// <summary>
+        /// Вывод строк и статистики их упоминаний
+        /// </summary>
+        /// <param name="strings">Строки и их упоминания</param>
+        void Print(IDictionary<string, int> strings);
+    }
+}
