9e01e4e358ca547c0b888cbc243d2d3e9f5e4081
Ellina Kuznetsova <ellinakuznetsova@mail.ru>
2021-02-10 05:42:59 +0300

    Task 4. Добавлен класс загрузчика и его вызов

---
+++ InternProject/Downloader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+
+namespace InternProject
+{
+    /// <summary>
+    /// Загрузчик
+    /// </summary>
+    public class Downloader : IDownloader
+    {
+        /// <summary>
+        /// Загрузка страницы
+        /// </summary>
+        /// <param name="url">Ссылка на страницу сайта</param>
+        /// <param name="path">Путь сохранения файла</param>
+        /// <returns>Успешно ли завершена загрузка</returns>
+        public bool DownloadPage(string url, string path)
+        {
+            WebClient client = new WebClient();
+            try
+            {
+                client.DownloadFile(url, path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Страница не скачана {ex.Message}");
+                return false;
+            }
+            
+            return true; 
+        }
+    }
+}
--- InternProject/Program.cs
+++ InternProject/Program.cs
@@ -10,10 +10,18 @@ namespace InternProject
             {
                 throw new Exception("Неверное количество аргументов");
             }
-            
+
             if (string.IsNullOrEmpty(args[0]))
             {
                 throw new Exception("Неверный формат строки");
+            }
+
+            IDownloader downloader = new Downloader();
+            string name = "downloadedFile.txt";
+
+            if (downloader.DownloadPage(args[0], name))
+            {
+                //smth
             }
         }
     }
